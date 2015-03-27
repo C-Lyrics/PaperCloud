de296a828c68cd2a8718999242fcc213af0934fb
seba-1511 <seba-1511@hotmail.com>
2015-03-27 16:39:34 -0700

    Added commands for protractor.

--- test/e2e/controllers/main.js
+++ test/e2e/controllers/main.js
@@ -1,22 +1,31 @@
 'use strict';
 
-describe('Controller: MainCtrl', function () {
+describe('Controller: MainCtrl', function() {
 
-  // load the controller's module
-  beforeEach(module('frontendApp'));
+    // load the controller's module
+    beforeEach(module('frontendApp'));
 
-  var MainCtrl,
-    scope;
+    var MainCtrl,
+        scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function($controller, $rootScope) {
+        scope = $rootScope.$new();
+        MainCtrl = $controller('MainCtrl', {
+            $scope: scope
+        });
+    }));
+
+    describe('Application Homepage', function() {
+        it('should display the application name', function() {
+            browser.get('http://localhost:9000');
+
+            var appName = element(by.css('h3.text-muted')); //using the CSS selector
+
+            expect(appName.getText())
+                .toEqual('PaperCloud');
+        });
+
     });
-  }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
 });
