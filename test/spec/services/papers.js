96d3a3de13bb2c56e36a5618491a80c9a2241a40
seba-1511 <seba-1511@hotmail.com>
2015-04-05 18:34:52 -0700

    Implemented tests for Papers service.

--- test/spec/services/papers.js
+++ test/spec/services/papers.js
@@ -17,21 +17,32 @@ describe('Service: Papers', function() {
     });
 
     it('should return the expected number of best words', function() {
-
+        var top = 3,
+            papers = [
+                'éalskd fjaésldkfj asdéfklj asdfélkasjd féalskdf',
+                'éalskd fjaésldkfj asdéfklj asdfélkasjd féalskdf',
+                'éalskd fjaésldkfj asdéfklj asdfélkasjd féalskdf',
+                'éalskd fjaésldkfj asdéfklj asdfélkasjd féalskdf',
+                'éalskd fjaésldkfj asdéfklj asdfélkasjd féalskdf',
+                'éalskd fjaésldkfj asdéfklj asdfélkasjd féalskdf',
+            ];
+        var words = Papers.getTopWords(top, words);
+        expect(words.length)
+            .toEqual(top);
     });
 
     it('should test countFrequency function', function() {
         var word1 = 'jaYz';
         var lyrics1 =
             'I hopped off the plane at L.A.X. With a dream and my cardigan Welcome to the land of fame excess (whoa), Am I gonna fit in? Jumped in the cab, Here I am for the first time Look to my right and I see the Hollywood sign This is all so crazy Everybody seems so famous My tummys turnin and Im feelin kinda home sick Too much pressure and Im nervous, Thats when the taxi man turned on the radio And a JayZ song was on And a JayZ song was on And a JayZ song was on';
-        var wordcount = Lyrics.countFrequency(word1, lyrics1);
+        var wordcount = Papers.countFrequency(word1, lyrics1);
         expect(wordcount)
             .toEqual(3);
 
         var word2 = 'ALl';
         var lyrics2 =
             'ALL I WANT IS YOU ALL I WANT IS YOU ALL I WANT IS YOU ALL I WANT IS YOU ALL I WANT IS YOU ALL I WANT IS YOU ALL I WANT IS YOU ALL I WANT IS YOU ALL I WANT IS YOU ALL I WANT IS YOU ALL I WANT IS YOU';
-        var wordcount2 = Lyrics.countFrequency(word2, lyrics2);
+        var wordcount2 = Papers.countFrequency(word2, lyrics2);
         expect(wordcount2)
             .toEqual(11);
     });
@@ -54,8 +65,11 @@ describe('Service: Papers', function() {
             text: 'ever',
             weight: 120
         }];
-        var frequentwords = Lyrics.selectMostFrequents(words, 4);
+        var frequentwords = Papers.selectMostFrequents(words, 4);
         //expect 'the' not to be in the list
+        expect(frequentwords.length)
+            .toEqual(4);
+
         expect(frequentwords)
             .toEqual([{
                 text: 'Kelsey',
