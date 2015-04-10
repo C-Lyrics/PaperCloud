1f881d058198d60fd67605c926ffe5afbf69b732
seba-1511 <seba-1511@hotmail.com>
2015-04-09 18:57:14 -0700

    Fixed format of paper list.

--- app/scripts/services/papers.js
+++ app/scripts/services/papers.js
@@ -89,21 +89,27 @@ angular.module('frontendApp')
                 title: 'XP Practices',
                 author: 'GJ Halfond',
                 content: 'After reviewing the suggested XP practices, I have decided that it will be more fair for all groups if I assign the new practices rather than allow groups to choose their own.  The following will be the additional three practices for groups to follow for Iteration 2.',
+            }, {
                 title: 'Green Eggs and Ham',
                 author: 'Seb Arnold',
                 content: '“Do you like green eggs and ham?” asks Sam-I-am in this Beginner Book by Dr. Seuss. In a house or with a mouse? In a boat or with a goat? On a train or in a tree? Sam keeps asking persistently. With unmistakable characters and signature rhymes, Dr. Seuss’s beloved favorite has cemented its place as a children’s classic. In this most famous of cumulative tales, the list of places to enjoy green eggs and ham, and friends to enjoy them with, gets longer and longer. Follow Sam-I-am as he insists that this unusual treat is indeed a delectable snack to be savored everywhere and in every way.',
+            }, {
                 title: 'Scrum',
                 author: 'Kelsey Fargas',
                 content: 'What is Scrum? Scrum is a way for teams to work together to develop a product. Product development, using Scrum, occurs in small pieces, with each piece building upon previously created pieces. Building products one small piece at a time encourages creativity and enables teams to respond to feedback and change, to build exactly and only what is needed.',
+            }, {
                 title: 'The Notebook',
                 author: 'Mark Krant',
                 content: 'In time, the hurt began to fade and it was easier to just let it go. At least I thought it was. But in every boy I met in the next few years, I found myself looking for you, and when the feelings got too strong, Id write you another letter. But I never sent them for fear of what I might find. By then, youd gone on with your life and I didnt want to think about you loving someone else. I wanted to remember us like we were that summer. I didnt ever want to lose that.',
+            }, {
                 title: 'Extreme Programming',
                 author: 'Justine Cocchi',
                 content: 'Extreme programming (XP) is an agile software development methodology used to implement software projects. This article details the practices used in this methodology. Extreme programming has 12 practices, grouped into four areas, derived from the best practices of software engineering.',
+            }, {
                 title: 'Agile Guiding Practices',
                 author: 'Milad Gueramin',
                 content: '1. Our highest priority is to satisfy the customer through early and continuous delivery of valuable software. 2. Welcome changing requirements, even late in development. Agile processes harness change for the customers competitive advantage.',
+            }, {
                 title: 'Equivalence Partitioning',
                 author: 'Jeff Kang',
                 content: 'Basic idea: to identify groups of test cases that may reveal same types of errors (e.g., erroneous handling of all inputs > 100) Partitioning the input domain into groups from which to derive test cases (also called equivalence classes). A group is a set of data whose components are likely to be treated homogeneously by the program. Ideal case: all test cases in a group have the same outcome',
@@ -132,43 +138,43 @@ angular.module('frontendApp')
                         link: ''
                     };
                 });
-                // return selectMostFrequents(words, nbTopWords);
+                return selectMostFrequents(words, nbTopWords);
 
                 /****** For testing purposes *****************/
                 // TODO: Replace and work with Server.prod and Papers.papersMockup
-                return [{
-                    text: 'asdf',
-                    weight: 1,
-                    link: '',
-                }, {
-                    text: 'asdfasdf',
-                    weight: 2,
-                    link: '',
-                }, {
-                    text: 'asdflkj',
-                    weight: 3,
-                    link: '',
-                }, {
-                    text: 'asdfoi',
-                    weight: 4,
-                    link: '',
-                }, {
-                    text: 'lkjasdf',
-                    weight: 5,
-                    link: '',
-                }, {
-                    text: 'asc',
-                    weight: 6,
-                    link: '',
-                }, {
-                    text: 'asc',
-                    weight: 7,
-                    link: '',
-                }, {
-                    text: 'oiuhokjl',
-                    weight: 8,
-                    link: '',
-                }, ];
+                //return [{
+                //text: 'asdf',
+                //weight: 1,
+                //link: '',
+                //}, {
+                //text: 'asdfasdf',
+                //weight: 2,
+                //link: '',
+                //}, {
+                //text: 'asdflkj',
+                //weight: 3,
+                //link: '',
+                //}, {
+                //text: 'asdfoi',
+                //weight: 4,
+                //link: '',
+                //}, {
+                //text: 'lkjasdf',
+                //weight: 5,
+                //link: '',
+                //}, {
+                //text: 'asc',
+                //weight: 6,
+                //link: '',
+                //}, {
+                //text: 'asc',
+                //weight: 7,
+                //link: '',
+                //}, {
+                //text: 'oiuhokjl',
+                //weight: 8,
+                //link: '',
+                //}, ];
 
             },
         };
