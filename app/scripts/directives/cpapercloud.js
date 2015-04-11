0fd66294f00bb18d415dfcd411ca77ced8262301
seba-1511 <seba-1511@hotmail.com>
2015-04-11 11:11:46 -0700

    Formatted according to our coding standards.

--- app/scripts/directives/cpapercloud.js
+++ app/scripts/directives/cpapercloud.js
@@ -7,34 +7,34 @@
  * # cPaperCloud
  */
 angular.module('frontendApp')
-.directive('cPaperCloud', function($compile) {
-    return {
-        templateUrl: 'views/directives/cPaperCloud.html',
-        restrict: 'EACM',
-        replace: false,
-        scope: {
-            words: '=words',
-        },
-        link: function postLink(scope, element, attrs) {
-            scope.clicks = 0;
-            var click_double = function(single, double, arg) {
-                scope.clicks++;
-                if (scope.clicks === 1) {
-                    setTimeout(function() {
-                        if (scope.clicks == 1) {
-                            single(arg);
-                        }
-                        else {
-                            double(arg);
-                        }
-                    }, 300);
-                }
-            };
-            scope.$watch('words', function(oldVal, newVal) {
+    .directive('cPaperCloud', function($compile) {
+        return {
+            templateUrl: 'views/directives/cPaperCloud.html',
+            restrict: 'EACM',
+            replace: false,
+            scope: {
+                words: '=words',
+            },
+            link: function postLink(scope, element, attrs) {
                 scope.clicks = 0;
-                if (scope.words && scope.words.length) {
-                    scope.wordsFormatted = scope.words.map(function(
-                        curr, idx) {
+                var click_double = function(single, double, arg) {
+                    scope.clicks++;
+                    if (scope.clicks === 1) {
+                        setTimeout(function() {
+                            if (scope.clicks == 1) {
+                                single(arg);
+                            }
+                            else {
+                                double(arg);
+                            }
+                        }, 300);
+                    }
+                };
+                scope.$watch('words', function(oldVal, newVal) {
+                    scope.clicks = 0;
+                    if (scope.words && scope.words.length) {
+                        scope.wordsFormatted = scope.words.map(function(
+                            curr, idx) {
                             curr.handlers = {
                                 click: function(event) {
                                     click_double(
@@ -42,7 +42,8 @@ angular.module('frontendApp')
                                             scope.redirectTo(curr.text);
                                         },
                                         function(event) {
-                                            scope.runNewSearch(curr.text);
+                                            scope.runNewSearch(curr
+                                                .text);
                                         },
                                         event
                                     );
@@ -50,17 +51,17 @@ angular.module('frontendApp')
                             };
                             return curr
                         });
-                }
-            });
-            scope.color = ['#FF0000', '#00FF00', '#0000FF', '#FF00FF'];
+                    }
+                });
+                scope.color = ['#FF0000', '#00FF00', '#0000FF', '#FF00FF'];
 
-            scope.redirectTo = function(word) {
-                alert('Redirecting for word: ' + word);
-            };
+                scope.redirectTo = function(word) {
+                    alert('Redirecting for word: ' + word);
+                };
 
-            scope.runNewSearch = function(word) {
-                alert('Generating new word cloud for: ' + word);
-            };
-        }
-    };
-});
+                scope.runNewSearch = function(word) {
+                    alert('Generating new word cloud for: ' + word);
+                };
+            }
+        };
+    });
--- app/scripts/services/papers.js
+++ app/scripts/services/papers.js
@@ -94,7 +94,7 @@ angular.module('frontendApp')
             extractWords: extractWords,
             countFrequency: countFrequency,
             selectMostFrequents: selectMostFrequents,
-            
+
             cached: [],
 
             // TODO: Create more fake papers
