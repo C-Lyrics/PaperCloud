e70255db213003abe90f8dd11ca5213c2132b675
seba-1511 <seba-1511@hotmail.com>
2015-04-11 10:55:41 -0700

    Implemented the click-double click functionality.

--- app/scripts/directives/cpapercloud.js
+++ app/scripts/directives/cpapercloud.js
@@ -7,39 +7,60 @@
  * # cPaperCloud
  */
 angular.module('frontendApp')
-    .directive('cPaperCloud', function($compile) {
-        return {
-            templateUrl: 'views/directives/cPaperCloud.html',
-            restrict: 'EACM',
-            replace: false,
-            scope: {
-                words: '=words',
-            },
-            link: function postLink(scope, element, attrs) {
-                scope.bg = 'white';
-                scope.$watch('words', function(oldVal, newVal) {
-                    if (scope.words && scope.words.length) {
-                        scope.wordsFormatted = scope.words.map(function(
-                            curr, idx) {
-                            /*
-                             * This click, link and double click thing requires more thoughts.
-                             */
-                            curr.html = {
-                                'ng-href': 'redirectTo',
-                                'ondblclick': 'redirectTo("dbl")',
+.directive('cPaperCloud', function($compile) {
+    return {
+        templateUrl: 'views/directives/cPaperCloud.html',
+        restrict: 'EACM',
+        replace: false,
+        scope: {
+            words: '=words',
+        },
+        link: function postLink(scope, element, attrs) {
+            scope.clicks = 0;
+            var click_double = function(single, double, arg) {
+                scope.clicks++;
+                if (scope.clicks === 1) {
+                    setTimeout(function() {
+                        if (scope.clicks == 1) {
+                            single(arg);
+                        }
+                        else {
+                            double(arg);
+                        }
+                    }, 300);
+                }
+            };
+            scope.$watch('words', function(oldVal, newVal) {
+                scope.clicks = 0;
+                if (scope.words && scope.words.length) {
+                    scope.wordsFormatted = scope.words.map(function(
+                        curr, idx) {
+                            curr.handlers = {
+                                click: function(event) {
+                                    click_double(
+                                        function(event) {
+                                            scope.redirectTo(curr.text);
+                                        },
+                                        function(event) {
+                                            scope.runNewSearch(curr.text);
+                                        },
+                                        event
+                                    );
+                                }
                             };
-                            curr.link = 'redirectTo';
                             return curr
                         });
-                    }
-                });
-                scope.width = 250;
-                scope.color = ['#FF0000', '#00FF00', '#0000FF', '#FF00FF'];
+                }
+            });
+            scope.color = ['#FF0000', '#00FF00', '#0000FF', '#FF00FF'];
 
-                scope.test = function(a, s, d, f) {
-                    debugger
-                    alert(a);
-                };
-            }
-        };
-    });
+            scope.redirectTo = function(word) {
+                alert('Redirecting for word: ' + word);
+            };
+
+            scope.runNewSearch = function(word) {
+                alert('Generating new word cloud for: ' + word);
+            };
+        }
+    };
+});
